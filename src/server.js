a9b4fae947076dc09ca28f15b56921c0ba61e48f
Ibrahim <ifarooq9@gmail.com>
2024-08-06 15:02:57 +0430

    Database set up

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
-const { PORT = 8000 } = process.env;
+require('dotenv').config()
 const app = require("./app");
+const connectDB = require("./db/conn.js")
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const port = process.env.PORT || 8000
+const listener = () => console.log(`Listening on Port ${port}!`);
+
+const startServer = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI) 
+        app.listen(port, listener);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer() 
