0e1e2035815077df23372e31d152f65f0707fc6b
Brittany Halterman <halterma@gmail.com>
2024-08-07 14:17:17 -0400

    Resolve merge conflicts and apply stashed changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,13 @@
+const jwt = require('jsonwebtoken');
+
 const register = async (req, res) => {
   res.send('register user');
 };
 
 const login = async (req, res) => {
   res.send('login user');
+
+  const token = jwt.sign({})
 };
 
 module.exports = {
