1095aeff044c9086d6d67081d7f566a231e8ada7
Brittany Halterman <halterma@gmail.com>
2024-08-07 20:22:09 -0400

    Set up Jasonwebtoken

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,9 +5,16 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { username, password } = req.body;
 
-  const token = jwt.sign({})
+  // temp
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'login user', token });
 };
 
 module.exports = {
