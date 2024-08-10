f66b8a481f01d876722d3f5dd1327f1072f93e68
Brittany Halterman <halterma@gmail.com>
2024-08-10 15:50:52 -0400

    Handle duplicate email errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -15,6 +15,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .join(', ')
     customError.statusCode = 400;
   };
+
+  if (err.code && err.code == 11000) {
+    customError.msg = `Duplicate value enetered for ${Object.keys(err.keyValue)} field, please choose another value`;
+    customError.statusCode = 400;
+  };
 };
 
 module.exports = errorHandlerMiddleware;
