de3c701fa4083ef113dd9235f8f59a4a429db295
Brittany Halterman <halterma@gmail.com>
2024-08-10 15:47:31 -0400

    Handle validation errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -7,6 +7,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later'
   };
+
+  if (err.name === 'ValidationError') {
+    console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ')
+    customError.statusCode = 400;
+  };
 };
 
 module.exports = errorHandlerMiddleware;
