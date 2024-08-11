cf770796549663e097ceff1b27d900f4318296ff
Ibrahim <ifarooq9@gmail.com>
2024-08-11 11:31:45 +0430

    Error added in register controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,7 +13,7 @@ const register = async (req, res) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      throw new BadRequestError('User already exists');
     }
 
     // Create a new user
