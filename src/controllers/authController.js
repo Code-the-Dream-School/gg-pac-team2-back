2f7be91c2cf95ac9f60ce727f5439f21c736725a
Ibrahim <ifarooq9@gmail.com>
2024-08-11 12:08:33 +0430

    Revert "Error added in register controller"
    This reverts commit cf770796549663e097ceff1b27d900f4318296ff.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,7 +13,7 @@ const register = async (req, res) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('User already exists');
+      return res.status(400).json({ message: 'User already exists' });
     }
 
     // Create a new user
