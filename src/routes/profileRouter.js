324b350b6ccc637838f293754187701b85d66c8e
Ibrahim <ifarooq9@gmail.com>
2024-08-20 13:30:29 +0430

    AllUsers and Delete Users feature api added

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const {
   readProfile,
-  updateProfile
+  updateProfile,
+  deleteProfile,
+  readAllProfiles
 } = require('../controllers/profileController');
 
 const authMiddleware = require('../middleware/authMiddleware')
 
-router.route('/').get(authMiddleware, readProfile).patch(authMiddleware, updateProfile);
+router.route('/').get(authMiddleware, readAllProfiles).get(authMiddleware, readProfile).patch(authMiddleware, updateProfile);
+router.route('/:id').delete(authMiddleware, deleteProfile)
 
 module.exports = router;
