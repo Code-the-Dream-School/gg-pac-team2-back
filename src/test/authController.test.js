919c6bf5b6a5d4224290bd00a1263bf9a26d2a79
Brittany Halterman <halterma@gmail.com>
2024-08-19 21:19:27 -0400

    Debug test for auth controller

--- src/test/authController.test.js
+++ src/test/authController.test.js
@@ -1,12 +1,35 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+const mongoose = require('mongoose');
 
 chai.use(chaiHttp);
 chai.should();
 
-describe('Authentication Controller - Register', () => {
+async function connectDB() {
+  const uri = process.env.MONGO_URI;
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+async function disconnectDB() {
+  await mongoose.connection.close();
+};
+
+describe('Authentication Controller - Register', function () {
+  before(async () => {
+    await connectDB();
+  });
+
+  after(async () => {
+    await disconnectDB();
+  });
+
   it('should register a new user and return a token', (done) => {
+    this.timeout(15000);
+
     chai
       .request(app)
       .post('/api/v1/auth/register')
@@ -16,6 +39,8 @@ describe('Authentication Controller - Register', () => {
         password: 'secret',
       })
       .end((err, res) => {
+        console.log('Error:', err);
+        console.log('Response:', res.body);
         res.should.have.status(201);
         res.body.should.be.an('object');
         res.body.should.have.property('user');
