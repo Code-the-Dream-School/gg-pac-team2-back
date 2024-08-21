b64b4b2819048448efc5189d10fede03568f052c
Ibrahim <ifarooq9@gmail.com>
2024-08-21 15:25:32 +0430

    necessary changes in router done

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -11,7 +11,8 @@ const {
 
 const authMiddleware = require('../middleware/authMiddleware')
 
-router.route('/').get(authMiddleware, readAllProfiles).get(authMiddleware, readProfile).patch(authMiddleware, updateProfile);
+router.route('/').get(authMiddleware, readProfile).patch(authMiddleware, updateProfile);
+router.route('/allprofiles').get(authMiddleware, readAllProfiles)
 router.route('/:id').delete(authMiddleware, deleteProfile).get(authMiddleware, viewProfileById)
 
 module.exports = router;
