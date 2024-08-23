81d5272268028d7b40a86b63612faa655a1a8777
Ibrahim Farooq <113907706+ifarooq09@users.noreply.github.com>
2024-08-23 18:44:49 +0430

    User Profile - readAllUsers and deleteUser (#10)
    * Set up profile router
    
    * Set up profile controller
    
    * Modify app.js to include profile router
    
    * Implement read and update operations for user profile
    
    * AllUsers and Delete Users feature api added
    
    * viewProfileById feaure api also added
    
    * necessary changes in router done
    
    ---------
    
    Co-authored-by: Brittany Halterman <halterma@gmail.com>

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,6 +1,12 @@
 const User = require('../models/usersModel');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError, NotFoundError } = require('../errors');
+const { BadRequestError, NotFoundError, ForbiddenError } = require('../errors');
+
+const readAllProfiles = async (req, res) => {
+  const users = await User.find({}).select('-password -tokens')
+
+  res.status(StatusCodes.OK).json({ users });
+}
 
 const readProfile = async (req, res) => {
   const { userId } = req.user;
@@ -13,6 +19,19 @@ const readProfile = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ user });
 };
+
+const viewProfileById = async (req, res) => {
+  const { id: profileId } = req.params
+
+  const user = await User.findById(profileId).select('-password -tokens')
+
+  if ( !user ) {
+    throw new NotFoundError(`No profile found for user with id ${profileId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ user });
+}
+
 
 const updateProfile = async (req, res) => {
   const {
@@ -37,7 +56,27 @@ const updateProfile = async (req, res) => {
   res.status(StatusCodes.OK).json({ user });
 };
 
+const deleteProfile = async (req, res) => {
+  const { userId } = req.user;
+  const { id: profileId } = req.params;
+
+  if ( userId != profileId ) {
+    throw new ForbiddenError('You are not authorized to delete this profile')
+  }
+
+  const user = await User.findByIdAndDelete(profileId)
+
+  if ( !user ) {
+    throw new NotFoundError(`No profile found for user with id ${profileId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "Profile deleted successfully "});
+}
+ 
 module.exports = {
   readProfile,
-  updateProfile
+  updateProfile,
+  deleteProfile,
+  readAllProfiles,
+  viewProfileById
 };
--- src/errors/index.js
+++ src/errors/index.js
@@ -2,10 +2,12 @@ const CustomAPIError = require('./customAPIError');
 const BadRequestError = require('./badRequest');
 const UnauthenticatedError = require('./unauthenticatedError');
 const NotFoundError = require('./notFound');
+const ForbiddenError = require('./forbidden')
 
 module.exports = {
   CustomAPIError,
   BadRequestError,
   UnauthenticatedError,
   NotFoundError,
+  ForbiddenError
 };
