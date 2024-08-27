e4b63d69a6e8080ace0f92847ca65d7d454a2586
Brittany Halterman <halterma@gmail.com>
2024-08-26 22:36:38 -0400

    Write unit test for update user controller

--- src/test/profileController.test.js
+++ src/test/profileController.test.js
@@ -11,24 +11,24 @@ async function connectDB() {
   const uri = process.env.MONGO_URI;
   await mongoose.connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
-};
+}
 
 async function disconnectDB() {
   await mongoose.connection.close();
-};
+}
 
 describe('Profile Controller', function () {
   let token;
 
-  before(async () => {
+  before(async function () {
     await connectDB();
 
     await User.create({
       parentName: 'Test User',
       email: 'test.user@test.com',
-      password: 'secret'
+      password: 'secret',
     });
 
     const res = await chai
@@ -36,7 +36,7 @@ describe('Profile Controller', function () {
       .post('/api/v1/auth/login')
       .send({
         email: 'test.user@test.com',
-        password: 'secret'
+        password: 'secret',
       });
 
     token = res.body.token;
@@ -48,7 +48,7 @@ describe('Profile Controller', function () {
 
   // Read Profile
   describe('GET /api/v1/profile', function () {
-    it('should return authenticated user\'s profile', async function () {
+    it("should return authenticated user's profile", async function () {
       this.timeout(15000);
 
       const res = await chai
@@ -58,14 +58,35 @@ describe('Profile Controller', function () {
 
       res.should.have.status(200);
       res.body.should.be.an('object');
-      res.body.user.should.have.property('email', 'test.user@test.com');
+      res.body.user.should.have.property(
+        'email',
+        'test.user@test.com'
+      );
     });
   });
 
   // Update Profile
   describe('PATCH /api/v1/profile', function () {
-    it('should update authenticated user\'s profile', async function () {
-      // Test
+    it("should update authenticated user's profile", async function () {
+      const res = await chai
+        .request(app)
+        .get('/api/v1/profile')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          parentName: 'Updated User',
+          email: 'updated.user@test.com',
+        });
+
+      res.should.have.status(200);
+      res.body.should.be.an('object');
+      res.body.user.should.have.property(
+        'parentName',
+        'Updated User'
+      );
+      res.body.user.should.have.property(
+        'email',
+        'updated.user@test.com'
+      );
     });
   });
 
@@ -90,5 +111,3 @@ describe('Profile Controller', function () {
     });
   });
 });
-
-
