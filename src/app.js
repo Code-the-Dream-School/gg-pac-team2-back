4f17815a35b354005bad456d4656efce7079d2e5
Brittany Halterman <halterma@gmail.com>
2024-09-02 19:17:37 -0400

    Add request router to app.js

--- src/app.js
+++ src/app.js
@@ -13,8 +13,9 @@ const logger = require('morgan');
 const mainRouter = require('./routes/mainRouter.js');
 const authRouter = require('./routes/authRouter.js');
 const profileRouter = require('./routes/profileRouter');
+const requestRouter = require('./routes/requestRouter');
 
- 
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -27,6 +28,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use('/api/v1', mainRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/profile', profileRouter);
+app.use('/api/v1/requests', requestRouter)
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
